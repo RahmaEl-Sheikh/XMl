d32f7a09a0a90c27b06868faa11e57233d280095
RahmaEl-Sheikh <eng.rahma.elsheikh@gmail.com>
2023-02-12 18:21:36 +0200

    first lab upload

---
+++ xml (1)/XML Library Configuratuin/src/xml/library/configuratuin/XMLLibraryConfiguratuin.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package xml.library.configuratuin;
+
+/**
+ *
+ * @author rahma
+ */
+public class XMLLibraryConfiguratuin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
